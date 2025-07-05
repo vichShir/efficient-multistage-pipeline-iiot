1122a85535fa72b6d77f7ecfe2a0c622f3e5834d
vichShir <victor.shirasuna@gmail.com>
2025-07-05 15:10:56 -0300

    Updated raspberry pi scripts

--- raspberry_pi/run_mb.sh
+++ raspberry_pi/run_mb.sh
@@ -2,5 +2,9 @@
 for seed in {0..40..10}
 do
     echo "Running seed ${seed}"
-    python run_inference.py --dataset datasets/test_features_markov-blanket_seed${seed}.csv --model models/mb_seed${seed}.pkl --save_filename test_inference_time_mb_seed${seed}.csv --num_exec 100
+    python run_inference.py \
+        --dataset datasets/test_features_markov-blanket_seed${seed}.csv \
+        --model models/mb_seed${seed}.pkl \
+        --save_filename test_inference_time_mb_seed${seed}.csv \
+        --num_exec 100
 done
